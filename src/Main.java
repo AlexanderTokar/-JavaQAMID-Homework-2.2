a50df708b454efeb818b14166da6135f47ff2b9c
Александр Токарь <alextokar@inbox.ru>
2022-06-19 18:35:41 +0300

    Final

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientAccount = 100; // Сумма на счете клиента
+        int amount = 1100; // Сумма пополнения
+        int bonus = amount / 100; // Формула расчета бонуса
+        if (amount <= 100) {
+            bonus = 0;
+        }
+        System.out.println("Ваш бонус: " + bonus + " рубля(ей)"); // Вывод количества бонусов
+        int totalScore = clientAccount + bonus + amount; // Сумма на счете клиента с учетом бонуса
+        System.out.println("Ваш счет: " + totalScore + " рубля(ей)"); // Вывод значения итоговой суммы на счете клиента
+    }
+
+}
